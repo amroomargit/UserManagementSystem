f956f46b8d33b37527e0d5436976b75b3f0e6ccc
Amro Omar <amroomargit@gmail.com>
2025-11-27 00:49:44 -0500

    Logging and AOP

---
+++ src/main/java/com/sword/usermanagementsystem/aop/LoggingAspect.java
@@ -0,0 +1,25 @@
+package com.sword.usermanagementsystem.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.sword.usermanagementsystem.controllers..*(..))")
+    public void logBeforeControllerMethods(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        Object[] args = joinPoint.getArgs();
+
+        log.info("-> Entering {}.{}() with args: {}",className,methodName,args);
+    }
+}
